f181e2b4bf51ee6b5d85ff81b3e97d77135bbf2a
msaaddev <mrsaadirfan@gmail.com>
2019-08-08 20:21:17 +0500

    📦NEW: router app

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import NavBar from "./components/navbar";
+import Products from "./components/products";
+import Posts from "./components/posts";
+import Home from "./components/home";
+import Dashboard from "./components/admin/dashboard";
+import ProductDetails from "./components/productDetails";
+import NotFound from "./components/notFound";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <div className="content">
+          <Switch>
+            <Route path="/products" component={Products} />
+            <Route path="/posts" component={Posts} />
+            <Route path="/admin" component={Dashboard} />
+            <Route path="/" exact component={Home} />
+          </Switch>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
